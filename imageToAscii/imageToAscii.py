1d8e587a8a27c79c3670b35e473c8436d74ccf87
Robert Sheldon <rsheldiii@gmail.com>
2012-08-16 12:25:03 -0400

    added Siglemic Tracker. no support for exclamation points in git

--- imageToAscii/imageToAscii.py
+++ imageToAscii/imageToAscii.py
@@ -3,24 +3,24 @@ import Image,sys,os
 
 
 
-def asciiArt():
-    for imfile in sys.argv[1:]:
-        im = Image.open(imfile)
-        name,extension = os.path.splitext(imfile)
-        xsize,ysize = im.size
-        pixels = im.load()
-        
-        f = open(name+".out","w")
-        
-        
-        for y in range(0,ysize):
-            for x in range(0,xsize):
-                total = sum(pixels[(x,y)])
-                if total == 765:
-                    f.write("w")
-                else: f.write("b")
-            f.write("\n")
-        f.close()
+    def asciiArt():
+        for imfile in sys.argv[1:]:
+            im = Image.open(imfile)
+            name,extension = os.path.splitext(imfile)
+            xsize,ysize = im.size
+            pixels = im.load()
+            
+            f = open(name+".out","w")
+            
+            
+            for y in range(0,ysize):
+                for x in range(0,xsize):
+                    total = sum(pixels[(x,y)])
+                    if total == 765:
+                        f.write("w")
+                    else: f.write("b")
+                f.write("\n")
+            f.close()
         
         
         
